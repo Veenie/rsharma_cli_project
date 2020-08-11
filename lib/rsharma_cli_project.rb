f08ed8b5fd5f64abd8d10e5cdbdcb4b2ebd6241d
Raveen K Sharma <raveen08@gmail.com>
2020-08-11 23:15:45 +0000

    initial setup

---
+++ lib/rsharma_cli_project.rb
@@ -0,0 +1,6 @@
+require "rsharma_cli_project/version"
+
+module RsharmaCliProject
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rsharma_cli_project/version.rb
@@ -0,0 +1,3 @@
+module RsharmaCliProject
+  VERSION = "0.1.0"
+end
