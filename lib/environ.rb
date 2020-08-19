abbb013cccf59ecbcd8c4b91caaf8f19d375047b
Raveen K Sharma <raveen08@gmail.com>
2020-08-19 20:18:13 +0000

    added pry to environ

--- lib/environ.rb
+++ lib/environ.rb
@@ -1,6 +1,8 @@
 require_relative "./rsharma_cli_project/version"
 require_relative "./rsharma_cli_project/cli"
 
+require 'pry'
+
 module RsharmaCliProject
   class Error < StandardError; end
   # Your code goes here...
