45a2900967f1573851ad4bf3e3d60f1f7363657b
Raveen K Sharma <raveen08@gmail.com>
2020-08-21 01:13:02 +0000

    added method cli file

--- lib/rsharma_cli_project/cli.rb
+++ lib/rsharma_cli_project/cli.rb
@@ -1,8 +1,21 @@
 
 
 class CLI
+  
   def call
     puts "Hello World!"
+    titles
+    list_titles
   end
-end
+
   
+  def titles
+    @titles = ['A', 'B', 'C']
+  end
+  
+  def list_titles
+    @titles.each_with_index do |title, index|
+      puts "#{index + 1}. #{title}"
+    end
+  end
+end  
