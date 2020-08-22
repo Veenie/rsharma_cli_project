56db4b01303be1f0815b66e8f01bb3b91ca2548b
Raveen K Sharma <raveen08@gmail.com>
2020-08-22 23:11:51 +0000

    Putting names and descriptions in arrays in scraper

--- lib/rsharma_cli_project/scraper.rb
+++ lib/rsharma_cli_project/scraper.rb
@@ -1,19 +1,30 @@
 class RsharmaCliProject::Scraper
   
+  @name = []
+  @descrip = []
   
   def self.scrape
    site = "https://en.wikipedia.org/wiki/List_of_The_Big_O_episodes"
    doc = Nokogiri::HTML(open(site))
-   episodes = doc.css("table.wikitable")
-   episodes.each do |ep|
-     name = ep.css("td.summary").text
-     description = ep.css("td.description").text
-     new = RsharmaCliProject::Episode.new(name, description)
+   titles = doc.css("td.summary")
+   titles.map do |t|
+     name = t.text
+     @name << name
      
-    end 
+   end
+   des = doc.css("td.description")
+   des.each do |d|
+     description = d.text
+     @descrip << description
+     
+    end
   end
   
   
+  
+  
+  
+
   
   
   
