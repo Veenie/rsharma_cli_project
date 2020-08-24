c351fd512667fdc787145c65c8207ac1b623bda7
Raveen K Sharma <raveen08@gmail.com>
2020-08-24 06:13:40 +0000

    fixed scraper, level was off

--- lib/rsharma_cli_project/scraper.rb
+++ lib/rsharma_cli_project/scraper.rb
@@ -6,16 +6,16 @@ class RsharmaCliProject::Scraper
   def self.scrape
    site = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html"
    doc = Nokogiri::HTML(open(site))
-   titles = doc.css("ol.row")
+   titles = doc.css("article.product_pod")
    
    titles.each do |x|
      name = x.css("a").text
      price = x.css("p.price_color").text
      RsharmaCliProject::Episode.new(name, price)
-     binding.pry
-    end
+     
    end
-   
+  end
+end   
    
    
    
